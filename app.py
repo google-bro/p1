122528b7a64f47e6119465e5aa5c82f2f70c4918
google-bro <83259724+google-bro@users.noreply.github.com>
2021-05-05 10:48:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def helloIndex():
-    return 'Hello World from Python Flask!'
+    return render_template("index.html")
 
 app.run(host='0.0.0.0', port=8080)
